8450499140ef8267e393c72739d1b94c9d7ebb4e
Alex Sayers <alex.sayers@gmail.com>
2012-10-27 14:45:18 +0100

    Make ruby installation conditional

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo "Installing ruby..."
-pacman -S ruby
-echo "Provisioning server..."
+if [ -z "$(which ruby)" ]; then
+  pacman -S ruby
+fi
 curl "https://raw.github.com/asayers/provision/master/provision.rb" | ruby
